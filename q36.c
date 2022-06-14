1004878f65c7f7ed3d681d2d58ab9354dd694ac4
mon-jai <91261297+mon-jai@users.noreply.github.com>
2022-06-14 22:03:41 +0800

    Update q36.c

--- q36.c
+++ q36.c
@@ -7,7 +7,7 @@ typedef struct linked_list_item {
   struct linked_list_item* next_item;
 } LinkListItem;
 
-LinkListItem* scan_and_push(LinkListItem** linked_list_end_ptr) {
+LinkListItem* push(LinkListItem** linked_list_head_ptr, LinkListItem** linked_list_end_ptr) {
   int value;
   scanf(" %d", &value);
 
@@ -15,10 +15,98 @@ LinkListItem* scan_and_push(LinkListItem** linked_list_end_ptr) {
   new_linked_list_item->value = value;
   new_linked_list_item->next_item = NULL;
 
+  if (*linked_list_head_ptr == NULL) *linked_list_head_ptr = new_linked_list_item;
   if (*linked_list_end_ptr != NULL) (*linked_list_end_ptr)->next_item = new_linked_list_item;
   *linked_list_end_ptr = new_linked_list_item;
 
   return new_linked_list_item;
+}
+
+void insert_after_item(LinkListItem** linked_list_head_ptr, LinkListItem** linked_list_end_ptr) {
+  int insert_after;
+  scanf(" %d", &insert_after);
+  int value;
+  scanf(" %d", &value);
+
+  if (*linked_list_head_ptr == NULL) return;
+
+  LinkListItem* item_to_insert_after;
+  LinkListItem* new_linked_list_item = malloc(sizeof(LinkListItem));
+  new_linked_list_item->value = value;
+
+  item_to_insert_after = *linked_list_head_ptr;
+  while (true) {
+    if (item_to_insert_after->value == insert_after) break;
+    if (item_to_insert_after->next_item == NULL) return;
+
+    item_to_insert_after = item_to_insert_after->next_item;
+  }
+
+  new_linked_list_item->next_item = item_to_insert_after->next_item;
+  item_to_insert_after->next_item = new_linked_list_item;
+  if (item_to_insert_after == *linked_list_end_ptr) *linked_list_end_ptr = new_linked_list_item;
+}
+
+void remove_item(LinkListItem** linked_list_head_ptr, LinkListItem** linked_list_end_ptr) {
+  int value_to_remove;
+  scanf(" %d", &value_to_remove);
+
+  if ((*linked_list_head_ptr)->value == value_to_remove) {
+
+    LinkListItem* next_item = (*linked_list_head_ptr)->next_item;
+    free(*linked_list_head_ptr);
+    *linked_list_head_ptr = next_item;
+
+  } else {
+
+    LinkListItem* item_before_item_to_remove = *linked_list_head_ptr;
+    while (true) {
+      if (item_before_item_to_remove->next_item == NULL) return;
+      if (item_before_item_to_remove->next_item->value == value_to_remove) break;
+
+      item_before_item_to_remove = item_before_item_to_remove->next_item;
+    }
+
+    LinkListItem* item_to_remove = item_before_item_to_remove->next_item;
+    item_before_item_to_remove->next_item = item_to_remove->next_item;
+    if (*linked_list_end_ptr == item_to_remove) *linked_list_end_ptr = item_before_item_to_remove;
+
+    free(item_to_remove);
+  }
+}
+
+void shift(LinkListItem** linked_list_head_ptr, LinkListItem** linked_list_end_ptr) {
+  if (*linked_list_head_ptr == NULL) return;
+
+  LinkListItem* second_item = (*linked_list_head_ptr)->next_item;
+  *linked_list_head_ptr = second_item;
+  if (second_item == NULL) *linked_list_end_ptr = NULL;
+
+  free(*linked_list_head_ptr);
+}
+
+void pop(LinkListItem** linked_list_head_ptr, LinkListItem** linked_list_end_ptr) {
+  if (*linked_list_head_ptr == NULL) return;
+
+  if (*linked_list_head_ptr == *linked_list_end_ptr) {
+
+    *linked_list_head_ptr = NULL;
+    *linked_list_end_ptr = NULL;
+
+  } else {
+
+    LinkListItem* item_before_last_item = *linked_list_head_ptr;
+    while (true) {
+      if (item_before_last_item->next_item == *linked_list_end_ptr) break;
+      item_before_last_item = item_before_last_item->next_item;
+    }
+
+    LinkListItem* linked_list_last_item = item_before_last_item->next_item;
+    item_before_last_item->next_item = NULL;
+    *linked_list_end_ptr = item_before_last_item;
+
+    free(*linked_list_end_ptr);
+  }
 }
 
 void print_linked_list(LinkListItem* linked_list_head) {
@@ -45,8 +133,7 @@ int main() {
   scanf("%d", &linked_list_size);
 
   for (int i = 0; i < linked_list_size; i++) {
-    LinkListItem* new_linked_list_item = scan_and_push(&linked_list_end);
-    if (i == 0) linked_list_head = new_linked_list_item;
+    push(&linked_list_head, &linked_list_end);
   }
   getchar();
 
@@ -56,101 +143,19 @@ int main() {
 
     printf("%d\n", action);
 
+    if (action == 1) push(&linked_list_head, &linked_list_end);
+    else if (action == 2) insert_after_item(&linked_list_head, &linked_list_end);
+    else if (action == 3) remove_item(&linked_list_head, &linked_list_end);
+
+    else if (action == 4) shift(&linked_list_head, &linked_list_end);
+    else if (action == 5) pop(&linked_list_head, &linked_list_end);
+
+    else if (action == 6) break;
+
+    printf("%p %p\n", linked_list_head, linked_list_end);
     print_linked_list(linked_list_head);
 
-    if (action == 1) {
-
-      scan_and_push(&linked_list_end);
-
-    } else if (action == 2) {
-
-      int insert_after;
-      scanf(" %d", &insert_after);
-      int value;
-      scanf(" %d", &value);
-
-      LinkListItem* linked_list_current = linked_list_head;
-      while (true) {
-        if (linked_list_current->value == insert_after) {
-          break;
-        } else if (linked_list_current->next_item == NULL) {
-          linked_list_current = NULL;
-          break;
-        }
-        linked_list_current = linked_list_current->next_item;
-      }
-
-      if (linked_list_current == NULL) continue;
-
-      LinkListItem* new_linked_list_item = malloc(sizeof(LinkListItem));
-      new_linked_list_item->value = value;
-      new_linked_list_item->next_item = linked_list_current->next_item;
-
-      linked_list_current->next_item = new_linked_list_item;
-      if (linked_list_current == linked_list_end) linked_list_end = new_linked_list_item;
-    } else if (action == 3) {
-
-      int value_to_remove;
-      scanf(" %d", &value_to_remove);
-
-      if (linked_list_head->value == value_to_remove) {
-        LinkListItem* next_item = linked_list_head->next_item;
-        free(linked_list_head);
-        linked_list_head = next_item;
-      } else {
-        LinkListItem* linked_list_current = linked_list_head;
-        while (true) {
-          if (linked_list_current->next_item == NULL) {
-            linked_list_current = NULL;
-            break;
-          } else if (linked_list_current->next_item->value == value_to_remove) {
-            break;
-          }
-
-          linked_list_current = linked_list_current->next_item;
-        }
-
-        if (linked_list_current == NULL) continue;
-
-        LinkListItem* item_to_remove = linked_list_current->next_item;
-        linked_list_current->next_item = item_to_remove->next_item;
-        free(item_to_remove);
-      }
-
-    } else if (action == 4) {
-
-      if (linked_list_head == linked_list_end) continue;
-
-      LinkListItem* next_item = linked_list_head->next_item;
-      free(linked_list_head);
-      linked_list_head = next_item;
-
-    } else if (action == 5) {
-
-      if (linked_list_head == linked_list_end) continue;
-
-      LinkListItem* linked_list_current = linked_list_head;
-      while (true) {
-        if (linked_list_current->next_item == linked_list_end) break;
-        linked_list_current = linked_list_current->next_item;
-      }
-
-      if (linked_list_current == linked_list_head) {
-        linked_list_head = NULL;
-      } else {
-        linked_list_current->next_item = NULL;
-        linked_list_end = linked_list_current;
-      }
-
-      free(linked_list_end);
-
-    } else if (action == 6) {
-      break;
-    }
-
-    if (linked_list_head == NULL) linked_list_end = NULL;
-
-    print_linked_list(linked_list_head);
+    printf("\n");
   }
 
   print_linked_list(linked_list_head);
